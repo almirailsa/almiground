7441b69a3e6d883e7601beb8759016921384efdf
morganing <42610629+morganing@users.noreply.github.com>
2018-09-05 17:09:40 +0200

    Create npm-deploy.sh

---
+++ scripts/npm-deploy.sh
@@ -0,0 +1,11 @@
+#!bin/ksh
+
+curl -H "Authorization: token ${1}" https://api.github.com
+
+git clone https://github.com/morganing/alminio.git scripts/npm
+
+cp -r src/alminio scripts/npm
+
+git commit -am "Auto-update from Travis CI"
+
+git push
