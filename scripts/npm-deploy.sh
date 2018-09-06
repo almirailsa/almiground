7f658f329481e68aebd9f573f9028aba9f1c3522
morganing <42610629+morganing@users.noreply.github.com>
2018-09-06 17:37:51 +0200

    Update npm-deploy.sh

--- scripts/npm-deploy.sh
+++ scripts/npm-deploy.sh
@@ -1,21 +1,5 @@
 #!bin/ksh
 
-#curl https://api.github.com/?access_token=$1
-
-#git clone https://github.com/morganing/alminio.git scripts/npm
-
-#cp -a src/alminio/. scripts/npm/
-
-#cd scripts/npm
-
-#git add .
-
-#git commit -m "Auto-update from Travis CI"
-
-#git push origin HEAD:master
- 
-#cd ../..
-
 mkdir scripts/npm
 
 cd scripts/npm
@@ -24,12 +8,10 @@ git init
 
 git pull https://${1}@github.com/morganing/alminio.git
 
-cp -a src/alminio/. scripts/npm/
+cp -a ../../src/alminio/. .
 
 git add .
 
 git commit -m "Auto-update from Travis CI"
 
-git push https://${1}@github.com/morganing/alminio.git
-
-
+git push --set-upstream https://${1}@github.com/morganing/alminio.git master
