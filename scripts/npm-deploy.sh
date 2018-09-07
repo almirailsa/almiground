29b526d5066e551194ed77cd6432170cb5723d7c
morganing <42610629+morganing@users.noreply.github.com>
2018-09-07 11:00:55 +0200

    Update npm-deploy.sh

--- scripts/npm-deploy.sh
+++ scripts/npm-deploy.sh
@@ -1,18 +1,24 @@
 #!bin/ksh
 
-mkdir scripts/npm
+#mkdir scripts/npm
 
-cd scripts/npm
+#cd scripts/npm
 
-git clone https://${1}:x-oauth-basic@github.com/morganing/alminio.git
+#git clone https://${1}:x-oauth-basic@github.com/morganing/alminio.git
 
-curl https://api.github.com/?access_token=$1
+#curl https://api.github.com/?access_token=$1
 
-cp -a ../../src/alminio/. .
+#cp -a ../../src/alminio/. .
 
-git add .
+#git add .
 
-git commit -m "Auto-update from Travis CI"
+#git commit -m "Auto-update from Travis CI"
 
-git push --set-upstream origin master
+#git push --set-upstream origin master
+
+echo "registry.npmjs.org/:_authToken=$1" > .npmrc
+
+tsc -p src/alminio/main.ts
+
+npm publish src/alminio/
 
